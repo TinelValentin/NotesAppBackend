285f8ac068759d6841f1f9a95b3c2bdb20914f18
TinelValentin <tinel_valentin@yahoo.com>
2022-03-21 16:59:32 +0200

    Put and delete
    -Put and delete for Owner
    -Update note base on owner and id
    -Delete note based on owner and id
    -Delete all notes of one owner

--- Notes-app/NotesApi/Controllers/NotesController.cs
+++ Notes-app/NotesApi/Controllers/NotesController.cs
@@ -13,12 +13,13 @@ namespace NotesApi.Controllers
     [Route("[controller]")]
     public class NotesController : ControllerBase
     {
-        static List<Note> _notes = new List<Note> { new Note { Id = Guid.NewGuid(), CategoryId = "1", OwnerId = Guid.NewGuid(), Title = "First Note", Description = "First Note Description" },
-        new Note { Id = Guid.NewGuid(), CategoryId = "1", OwnerId = Guid.NewGuid(), Title = "Second Note", Description = "Second Note Description" },
-        new Note { Id = Guid.NewGuid(), CategoryId = "1", OwnerId = Guid.NewGuid(), Title = "Third Note", Description = "Third Note Description" },
-        new Note { Id = Guid.NewGuid(), CategoryId = "1", OwnerId = Guid.NewGuid(), Title = "Fourth Note", Description = "Fourth Note Description" },
-        new Note { Id = Guid.NewGuid(), CategoryId = "1", OwnerId = Guid.NewGuid(), Title = "Fifth Note", Description = "Fifth Note Description" }
+        private static List<Note> _notes = new List<Note> { new Note { Id = new Guid("00000000-0000-0000-0000-000000000001"), CategoryId = "1", OwnerId = new Guid("00000000-0000-0000-0000-000000000001"), Title = "First Note", Description = "First Note Description" },
+        new Note { Id = new Guid("00000000-0000-0000-0000-000000000002"), CategoryId = "1", OwnerId = new Guid("00000000-0000-0000-0000-000000000001"), Title = "Second Note", Description = "Second Note Description" },
+        new Note { Id = new Guid("00000000-0000-0000-0000-000000000003"), CategoryId = "1", OwnerId = new Guid("00000000-0000-0000-0000-000000000001"), Title = "Third Note", Description = "Third Note Description" },
+        new Note { Id = new Guid("00000000-0000-0000-0000-000000000004"), CategoryId = "1", OwnerId = new Guid("00000000-0000-0000-0000-000000000001"), Title = "Fourth Note", Description = "Fourth Note Description" },
+        new Note { Id = new Guid("00000000-0000-0000-0000-000000000005"), CategoryId = "1", OwnerId = new Guid("00000000-0000-0000-0000-000000000001"), Title = "Fifth Note", Description = "Fifth Note Description" }
         };
+
 
         public NotesController() { }
 
@@ -27,8 +28,10 @@ namespace NotesApi.Controllers
         [HttpPost]
         public IActionResult Post([FromBody] Note note)
         {
+            if (note == null)
+                return BadRequest("Note can't be null!");
             _notes.Add(note);
-            return CreatedAtRoute("getNoteById",note.Id, note);
+            return CreatedAtRoute("getNoteById", note.Id, note);
         }
 
         [HttpGet]
@@ -61,6 +64,100 @@ namespace NotesApi.Controllers
                     notesById.Add(note);
             }
             return Ok(notesById);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult UpdateNote(Guid id, [FromBody] Note note)
+        {
+            if (note == null)
+            {
+
+                return BadRequest("Note can't be null!");
+
+            }
+            int index = _notes.FindIndex(n => n.Id == id);
+            if (index == -1)
+            {
+                return Post(note);
+                //return NotFound("Id dosen't exist!");
+            }
+            _notes[index] = note;
+            _notes[index].Id = id;
+            return Ok(_notes);
+        }
+
+        [HttpPut("{noteId}/{ownerId}")]
+        public IActionResult updateByNoteAndOwner(Guid noteId, Guid ownerId, Note newNote)
+
+        {
+            if (noteId == null || ownerId == null)
+            {
+                return BadRequest("One id is null!");
+            }
+
+            int index = _notes.FindIndex(n => n.Id == noteId && n.OwnerId == ownerId);
+
+            if (index == -1)
+            {
+                return Post(newNote);
+            }
+
+            _notes[index] = newNote;
+            return Ok(_notes);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteNote(Guid id)
+        {
+            if (id == null)
+            {
+                return BadRequest("id can't be null!");
+            }
+            int index = _notes.FindIndex(n => n.Id == id);
+            if (index == -1)
+            {
+                return NotFound("Note dosen't exist!");
+            }
+            _notes.RemoveAt(index);
+
+            return Ok(_notes);
+        }
+
+        [HttpDelete("{noteId}/{ownerId}")]
+        public IActionResult DeleteNote(Guid noteId, Guid ownerId)
+        {
+            if (noteId == null || ownerId == null)
+            {
+                return BadRequest("id can't be null!");
+            }
+            int index = _notes.FindIndex(n => n.Id == noteId && n.OwnerId == ownerId);
+            if (index == -1)
+            {
+                return NotFound("Note dosen't exist!");
+            }
+            _notes.RemoveAt(index);
+
+            return Ok(_notes);
+        }
+
+        [HttpDelete("owner/{ownerId}")]
+        public IActionResult deleteAllNotes(Guid ownerId)
+        {
+            if(ownerId==null)
+            {
+                return BadRequest("id can't ben null!");
+
+            }
+
+            int index = _notes.FindIndex(n => n.OwnerId == ownerId);
+
+            if(index==-1)
+            {
+                return NotFound("Note doesn't exist!");
+            }
+
+            _notes.RemoveAt(index);
+            return Ok(_notes);
         }
     }
 }
--- Notes-app/NotesApi/Controllers/OwnerController.cs
+++ Notes-app/NotesApi/Controllers/OwnerController.cs
@@ -11,16 +11,21 @@ namespace NotesApi.Controllers
     [ApiController]
     public class OwnerController : ControllerBase
     {
-        static List<Owner> owners = new List<Owner>();
+        static List<Owner> owners = new List<Owner>
+        {
+           new Owner{ Id = new Guid("00000000-0000-0000-0000-000000000001"), Name= "first" },
+           new Owner{ Id = new Guid("00000000-0000-0000-0000-000000000002"), Name= "second" },
+           new Owner{ Id = new Guid("00000000-0000-0000-0000-000000000003"), Name= "third" }
+        };
         [HttpGet]
         public IActionResult GetOwner(Guid id)
         {
-            foreach(Owner owner in owners)
+            foreach (Owner owner in owners)
             {
                 if (owner.Id == id)
                     return Ok(owner);
             }
-            return BadRequest(id);
+            return BadRequest("id does not exist!");
         }
 
         [HttpPost]
@@ -28,6 +33,48 @@ namespace NotesApi.Controllers
         {
             owners.Add(newOwner);
             return Ok(newOwner);
+        }
+
+        [HttpDelete("{ownerId}")]
+        public IActionResult DeleteOwner(Guid ownerId)
+        {
+            if (ownerId == null)
+            {
+                return BadRequest("Id is null!");
+            }
+
+            int index = owners.FindIndex(n => n.Id == ownerId);
+            if (index == -1)
+            {
+                return NotFound("id dosen't exist!");
+            }
+
+            owners.RemoveAt(index);
+            return Ok(owners);
+        }
+
+
+        [HttpPut("{ownerId}")]
+        public IActionResult updateOwner(Guid ownerId, Owner newOwner)
+        {
+            if(ownerId==null)
+            {
+                return BadRequest("owner id can't be null!");
+            }
+            if(newOwner==null)
+            {
+                return BadRequest("new Owner can't be null");
+
+            }
+
+            int index = owners.FindIndex(n => n.Id == ownerId);
+            if(index==-1)
+            {
+                return BadRequest("Owner does not exist!");
+            }
+
+            owners[index] = newOwner;
+            return Ok(owners);
         }
     }
 }
